4e52909d0393597a9f931f1daf896288a83d5d4a
LLT21 <l.lt@fidesoft.com>
2022-01-02 20:07:30 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace aottest
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+
+         using (SqlConnection sqlConnection = new SqlConnection("server=xxx.database.windows.net;user id=xxx;password=xxx;Initial Catalog=appMpower;Connection Timeout=120;"))
+         {
+            sqlConnection.Open();
+            sqlConnection.Close();
+         }
+      }
+   }
+}
